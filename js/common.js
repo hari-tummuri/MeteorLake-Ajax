22bef5bd58a51864910babb21996562556870bd9
hari-tummuri1 <tummuri.hari1@wipro.com>
2024-03-06 10:24:38 +0530

    version 7

--- js/common.js
+++ js/common.js
@@ -299,7 +299,7 @@ var aCaseItems = [
     status: "closed",
     summary:
       "The Civil Appeal No. 2728 of 1972 revolves around the question of whether there was any material evidence to justify the finding that a sum of Rs. 1,07,350 was remitted by the appellant firm, M/s. Kishinchand Chellaram, from Madras to Bombay, and that it represented the undisclosed income of the firm.\nThe firm had challenged the decision of the Bombay High Court, which had upheld the Income Tax Officer's view that the firm failed to satisfactorily explain the source of the remittance, resulting in it being added to the firm's taxable income.\nHowever, the Supreme Court ruled that the evidence relied upon by the Income Tax Officer and the High Court, namely two letters from the Punjab National Bank Limited, did not constitute material evidence justifying the decision. \nThe court highlighted that the letters were not supported by any documents or papers and were based on hearsay. \nThe Supreme Court concluded that the highest evidence that could be established was that an employee of the firm remitted the amount to another employee, but it didn't necessarily imply that the amount belonged to the firm. \nThe burden was on the revenue to prove that the remitted amount belonged to the firm, which they failed to do.\n The Supreme Court thus allowed the appeal, setting aside the judgement of the High Court.",
-    files: "2728.pdf",
+    files: "judgement_2728.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -311,7 +311,7 @@ var aCaseItems = [
     status: "next hearing",
     summary:
       "The appeal case number 910 of 1970 concerns the election of Jugal Kishore Patnaik to the Orissa Legislative Assembly from Bhadrak constituency, which was declared void by the Orissa High Court due to an election petition filed by Ratnakar Mohanty.\nThe High Court found that Mohanty's nomination papers had been improperly rejected by the Returning Officer, due to an objection that Mohanty was disqualified under section 9A of the Representation of the People Act, 1951, for having contracts with the Government of Orissa.\n The Supreme Court of India dismissed the appeal, upholding the High Court's decision, stating that Mohanty's contracts were not personal, but were undertaken as the Sarpanch of Rahanj Gram Panchayat, and therefore, he was not disqualified from running for election. \nThe Supreme Court's judgment highlighted the need for clearer electoral regulations to prevent such misunderstandings from occurring.",
-    files: "3529.pdf",
+    files: "judgement_3529.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -323,7 +323,7 @@ var aCaseItems = [
     status: "closed",
     summary:
       "The Civil Appeal No. 2005 of 1978 was filed under section 123 of the Jammu and Kashmir Representation of People Act, 1957, challenging the dismissal of an election petition due to non-compliance with section 89(3) of the Act.\n The appellant and respondent were candidates in the 1977 general election for the Handwara Assembly Constituency, with the respondent being declared the successful candidate. \nThe appellant subsequently filed an election petition challenging the validity of the respondent's election on various grounds.\n The High Court dismissed the petition on the grounds that the appellant had not complied with section 89(3) of the Act, which requires that every copy of the election petition be attested by the petitioner under his own signature to be a true copy of the petition.\n The Supreme Court upheld the High Court's decision, stating that the requirement for the petitioner to attest the copies of the petition under his own signature is mandatory, and non-compliance results in the dismissal of the petition as per section 94 of the Act. The appeal was therefore dismissed.",
-    files: "4120.pdf",
+    files: "judgement_4120.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -335,7 +335,7 @@ var aCaseItems = [
     status: "dismissal of suit",
     summary:
       "The case, Appeal Nos. 266 267 of 1993, centers on a charge memorandum issued against the respondent, who was working as an Income Tax Officer, Muktsar in 1982-83.\n The memorandum alleges that the respondent completed certain assessments in an irregular manner, hastily, and with the intent to confer undue favors on the assessees.\n The Central Administrative Tribunal Principal Bench, New Delhi ruled in favor of the respondent and quashed the charge memorandum, leading to the current appeal. \nThe Supreme Court of India reversed the Tribunal's decision, upholding the validity of the charge memorandum.\n The Court concluded that disciplinary action can be taken in cases where the officer had acted in a manner that reflects negatively on his reputation for integrity, good faith or devotion to duty, or if there's evidence of recklessness, misconduct, negligence or undue favoritism in the discharge of his duty. \nThe court emphasized that the disciplinary action is not based on the correctness or legality of the respondent's decision, but on his conduct in discharging his duties.\n The court made it clear that the respondent can put forth all defenses open to him in the departmental inquiry which will be considered on its merit.",
-    files: "6991.pdf",
+    files: "judgement_6991.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -347,7 +347,7 @@ var aCaseItems = [
     status: "dismissal of suit",
     summary:
       "The petition under Article 32 of the Indian Constitution questions the validity of Regulation 7 along with Schedule I of State Bank of Patiala (Officers') Service Regulations,1979.\n It concerns the placement and fitment of existing officers in the new grades and scales of pay. The State Bank of Patiala, a subsidiary of the State Bank of India, revised and restructured pay scales based on the recommendations of the Pillai Committee.\nThe petitioners, Sri Tarsem Lal Gautam and Sri C. V. Madan, who are Grade A officers, challenged the classification of existing officers into different categories for fitment in the revised pay scales based solely on the date of their promotion as arbitrary and violative of Article 14 of the Constitution of India. \nThe bank argued that the Regulations did not bring about a mere revision of pay, but a rationalisation, standardisation and restructuring of the whole administrative set-up of the management cadres.\n The court ruled that the principle of classification among the existing officers for purposes of fitment was not unreasonable and arbitrary, and therefore, not violative of Article 14. The petition was dismissed.",
-    files: "5871.pdf",
+    files: "judgement_5871.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -359,7 +359,7 @@ var aCaseItems = [
     status: "closed",
     summary:
       "The appeals concern the valuation of shares of Mafatlal Gagalbhai Pvt. Ltd., an investment company. \nThe appellant, Pritam Singh, was elected to the Haryana State Legislative Assembly in 1972, but his election was challenged by respondent Balbir Singh on the grounds that Singh committed corrupt practices, including the hiring of vehicles for free conveyance of electors to polling stations.\n The main issue was the method of valuation of the company's shares. The Gift Tax and Wealth Tax officers used the break-up method, which the appellant contested, advocating for the profit-earning method instead.\n The Appellate Assistant Commissioner applied a combination of both methods, resulting in a higher valuation than the appellant claimed.\n The appellant appealed to the Tribunal, which ruled in favor of the profit-earning method, rejecting the combination method. The Revenue appealed to the Supreme Court, arguing that the High Court should have called for a reference on the question of the appropriate method of valuation.\n The Supreme Court dismissed the appeal, upholding the Tribunal's decision. The court stated that the profit-earning method was the appropriate method for valuing shares of a going concern and that the combination of the two methods was not a valid principle of valuation.\n The court also dismissed the Revenue's argument that the break-up method should apply due to a rule in the Gift Tax Act, as the question was not raised before the Tribunal.",
-    files: "4136.pdf",
+    files: "judgement_4136.pdf",
     email: "naveen@gmail.com",
     client: "Adarsh TR",
   },
@@ -413,7 +413,7 @@ function DoAuth() {
       .eq(0)
       .html("<img width='230'  src='" + aUserDp + "'/>");
     $("[for=check01]").html(
-      "<img  height='30' style='border-radius: 50%;margin-right:10px' src='" +
+      "<img  height='40' style='border-radius: 50%;margin-right:10px' src='" +
         aUserDp +
         "'/>" +
         aUserNm
